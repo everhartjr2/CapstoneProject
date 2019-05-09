dc87bec6a37586b4a4b5aa78116945eef8099878
Jake Everhart <31965284+everhartjr2@users.noreply.github.com>
2019-05-09 14:37:31 -0400

    Add files via upload

---
+++ AppDelegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace MasterDetailPageNavigation.iOS
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init ();
+            AiForms.Renderers.iOS.SettingsViewInit.Init();
+            Syncfusion.SfPdfViewer.XForms.iOS.SfPdfDocumentViewRenderer.Init();
+            Syncfusion.SfRangeSlider.XForms.iOS.SfRangeSliderRenderer.Init();
+
+            LoadApplication (new App ());
+
+			return base.FinishedLaunching (app, options);
+		}
+	}
+}
+
